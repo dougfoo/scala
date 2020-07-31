0a6d5f627b1bb816f81fa8775ad2bf24ed3dc39a
Doug Foo <doug.cha@gmail.com>
2020-07-31 18:03:25 +0900

    reset to remove big file

--- hw6/forcomp/src/main/scala/forcomp/Anagrams.scala
+++ hw6/forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -83,6 +83,14 @@ object Anagrams extends AnagramsInterface {
    *  Note that the order of the occurrence list subsets does not matter -- the subsets
    *  in the example above could have been displayed in some other order.
    */
+
+  def combinations2(occurrences:Occurrences): List[Occurrences] = {
+    occurrences.foldRight(List[Occurrences](Nil))((occ, acc) => acc ++ (for {
+      comb <- acc
+      i <- 1 to occ._2
+    } yield (occ._1, i) :: comb))
+  }
+
   def combinations(occurrences: Occurrences): List[Occurrences] = {
     /*
     Yes my ugly implementation... this is what I found after, very elegant...:
